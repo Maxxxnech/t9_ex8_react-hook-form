e36470ceacee1cd39ac00031a2398d8ec14b7b94
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-02-12 20:01:45 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from 'react';
 import content from "./static";
 import InputAndLabel from "./components/InputAndLabel";
 // Хук для состояния формы
